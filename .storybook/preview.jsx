dabcbc38574c11077deb5dab78fddcd363323ed1
ikwerre-dev <investorhonour@gmail.com>
2024-10-17 07:05:38 +0100

    initial deployment

---
+++ .storybook/preview.jsx
@@ -0,0 +1,24 @@
+import { Title, Subtitle, Description, Primary, Controls, Stories } from '@storybook/blocks';
+
+export default {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Controls />
+          <Stories />
+        </>
+      ),
+    },
+  },
+};
---
+++ src/pages/(events)/FindEvent.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+function FindEvent() {
+
+    return (
+        <>
+            <p>        Find Event Page
+            </p>
+
+        </>
+    )
+}
+
+export default FindEvent;
