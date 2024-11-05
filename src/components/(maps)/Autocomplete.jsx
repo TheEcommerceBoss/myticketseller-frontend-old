751ec9c3953a7fe4028613d18fc30c0a148486a4
ikwerre-dev <investorhonour@gmail.com>
2024-11-05 20:23:42 +0100

    updated

--- src/components/(maps)/Autocomplete.jsx
+++ src/components/(maps)/Autocomplete.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { LoadScript, Autocomplete } from '@react-google-maps/api';
+import { useJsApiLoader, Autocomplete } from '@react-google-maps/api';
 
 const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_MAP_API;
 
 const MapAutocomplete = ({ onAddressSelect }) => {
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+        libraries: ['places'],
+    });
+
     const [autocomplete, setAutocomplete] = useState(null);
     const [address, setAddress] = useState('');
 
@@ -16,22 +21,22 @@ const MapAutocomplete = ({ onAddressSelect }) => {
         }
     };
 
+    if (!isLoaded) return <div>Loading...</div>;
+
     return (
-        <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={['places']}>
-            <Autocomplete
-                onLoad={(autocompleteInstance) => setAutocomplete(autocompleteInstance)}
-                onPlaceChanged={handlePlaceChanged}
-            >
-                <input
-                    type="text"
-                    placeholder="Enter event location"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                    className="p-3 w-full bg-transparent border rounded-lg"
-                    style={{ width: '100%' }}
-                />
-            </Autocomplete>
-        </LoadScript>
+        <Autocomplete
+            onLoad={(autocompleteInstance) => setAutocomplete(autocompleteInstance)}
+            onPlaceChanged={handlePlaceChanged}
+        >
+            <input
+                type="text"
+                placeholder="Enter event location"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className="p-3 w-full bg-transparent border rounded-lg"
+                style={{ width: '100%' }}
+            />
+        </Autocomplete>
     );
 };
 
