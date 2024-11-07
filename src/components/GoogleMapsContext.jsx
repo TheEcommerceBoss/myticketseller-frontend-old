0c3a047d0fc991c20640fa73c721e55320af4f6d
ikwerre-dev <investorhonour@gmail.com>
2024-11-07 10:47:37 +0100

    fixed the google map components

--- src/components/GoogleMapsContext.jsx
+++ src/components/GoogleMapsContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import { LoadScript } from '@react-google-maps/api';
 
 const GoogleMapsContext = createContext();
@@ -7,14 +7,21 @@ export const GoogleMapsProvider = ({ children }) => {
   const apiKey = 'AIzaSyC73yaRGGiQ-W1qpni-3WlKJJ3A1vWtmUs'; 
   const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    // Check if Google Maps script is already loaded
+    if (window.google && window.google.maps) {
+      setIsLoading(false); // Skip loading if the API is already loaded
+    }
+  }, []);
+
   return (
     <GoogleMapsContext.Provider value={{ apiKey, isLoading }}>
-      <LoadScript 
+      <LoadScript
         googleMapsApiKey={apiKey}
         onLoad={() => setIsLoading(false)}
         onError={() => setIsLoading(false)} // handle potential errors as well
       >
-        { children}
+        {children}
       </LoadScript>
     </GoogleMapsContext.Provider>
   );
