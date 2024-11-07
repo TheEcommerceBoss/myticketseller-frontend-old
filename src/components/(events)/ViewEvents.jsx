f06ff663d9c9180efabb8a9610ca2fb9f7e83020
ikwerre-dev <investorhonour@gmail.com>
2024-11-07 11:18:43 +0100

    updated

--- src/components/(events)/ViewEvents.jsx
+++ src/components/(events)/ViewEvents.jsx
@@ -398,7 +398,7 @@ function ViewEventComponent({ variation }) {
                                 </h2>
                                 <p className={`mt-2 ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
                                     {eventDetails.additional_info.special_guests.split(',').map((guest, index) => (
-                                        <span key={index} className="bg-gray-100 text-gray-600 px-4 py-2 mr-2 rounded-full text-sm">
+                                        <span key={index} className={`${theme === 'dark' ? 'bg-gray-100 text-gray-600' : 'bg-orange-500 text-white'}  px-4 py-2 mr-2 rounded-full text-sm`}>
                                             {guest.trim()}
                                             {index < eventDetails.additional_info.special_guests.split(',').length - 1 && ' '}
                                         </span>
