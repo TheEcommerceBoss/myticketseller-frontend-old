eb5c4806bb1390cbfe0e5160da86f48091248d9b
ikwerre-dev <investorhonour@gmail.com>
2024-11-11 11:26:30 +0100

    updated

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -22,6 +22,7 @@ export const AuthProvider = ({ children }) => {
           });
           // console.log(token)
           setUserData(response.data);
+          console.log(response.data);
         } catch (error) {
           console.error("Failed to fetch user:", error);
 
