b430e3c4385946b65c25f6e6343414bd5b915b67
ikwerre-dev <investorhonour@gmail.com>
2024-12-08 18:20:48 +0100

    updated

--- src/components/(events)/ViewEvents.jsx
+++ src/components/(events)/ViewEvents.jsx
@@ -17,6 +17,7 @@ import { useParams } from "react-router-dom";
 import { GoogleMapsProvider } from '../GoogleMapsContext';
 import SimpleMap from '../props/Map';
 import Swal from 'sweetalert2';
+import { Helmet } from "react-helmet";
 
 import axios from 'axios';
 
@@ -131,12 +132,12 @@ const HostInfo = ({ theme, eventDetails }) => (
                     </svg>
                 </a>
                 }
-               {eventDetails.user.tiktok &&  <a href={eventDetails.user.tiktok} target="_blank" className="hover:opacity-75">
+                {eventDetails.user.tiktok && <a href={eventDetails.user.tiktok} target="_blank" className="hover:opacity-75">
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
                     </svg>
                 </a>}
-                {eventDetails.user.twitter &&   <a href={eventDetails.user.twitter} target="_blank" className="hover:opacity-75">
+                {eventDetails.user.twitter && <a href={eventDetails.user.twitter} target="_blank" className="hover:opacity-75">
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
                     </svg>
@@ -436,6 +437,13 @@ function ViewEventComponent({ variation }) {
                     </>
                 ) :
                     <div className="px-3 md:px-0">
+                        <Helmet>
+                            <title>{eventDetails.event_title}</title>
+                            <meta property="og:title" content={eventDetails.event_title} />
+                            <meta property="og:image" content={eventDetails.event_image} />
+                            <meta property="og:description" content="Check out this event!" />
+                            <meta property="og:type" content="website" />
+                        </Helmet>
                         <div className="flex flex-col  items-center p-1 md:p-5">
                             {eventDetails.event_image && <img src={eventDetails.event_image} className="w-full h-[30rem] object-cover  max-w-7xl mb-5 rounded-xl" alt="" />}
                         </div>
