70abdfe3904f4fd8716c97701cad3b107c650a6d
ikwerre-dev <investorhonour@gmail.com>
2024-12-08 18:13:39 +0100

    updated view events

--- src/components/(events)/ViewEvents.jsx
+++ src/components/(events)/ViewEvents.jsx
@@ -109,7 +109,7 @@ const Header = ({ theme, eventDetails, ticketDetails, id }) => {
 
 // Host Info Component
 const HostInfo = ({ theme, eventDetails }) => (
-    <div className={`lg:w-1/2 rounded-lg p-6 shadow-sm mb-8 ${theme === 'dark' ? 'bg-[#000]' : 'bg-white '}`}>
+    <div className={`lg:w-1/2 rounded-lg p-6 shadow-sm mb-8 ${theme === 'dark' ? 'bg-[#121212]' : 'bg-gray-100 '}`}>
         <div className="flex flex-col md:flex-row  md:items-center gap-4">
             <div className="flex items-center gap-4">
                 <img
@@ -125,21 +125,23 @@ const HostInfo = ({ theme, eventDetails }) => (
                 </div>
             </div>
             <div className="flex gap-4">
-                <a href={eventDetails.user.instagram} target="_blank" className="hover:opacity-75">
+                {eventDetails.user.instagram && <a href={eventDetails.user.instagram} target="_blank" className="hover:opacity-75">
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                     </svg>
                 </a>
-                <a href={eventDetails.user.tiktok} target="_blank" className="hover:opacity-75">
+                }
+               {eventDetails.user.tiktok &&  <a href={eventDetails.user.tiktok} target="_blank" className="hover:opacity-75">
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
                     </svg>
-                </a>
-                <a href={eventDetails.user.twitter} target="_blank" className="hover:opacity-75">
+                </a>}
+                {eventDetails.user.twitter &&   <a href={eventDetails.user.twitter} target="_blank" className="hover:opacity-75">
                     <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
                     </svg>
                 </a>
+                }
             </div>
         </div>
     </div>
@@ -386,12 +388,12 @@ function ViewEventComponent({ variation }) {
 
     ];
 
-    const unlockevent = () =>{
-        if(pin == trialPassword){
+    const unlockevent = () => {
+        if (pin == trialPassword) {
             setLock('unlocked')
             setTimeout(() => setIsLocked(false), 500);
             setTimeout(() => setLock('lock'), 500);
-        }else{
+        } else {
             setLock('lock')
             Swal.fire('Error', 'Wrong Event Password', 'error');
 
@@ -417,7 +419,7 @@ function ViewEventComponent({ variation }) {
                         <div className={`flex flex-col items-center justify-center  px-5`}>
                             <div className={`${theme === 'dark' ? 'bg-orange-500 text-gray-600' : 'bg-orange-500 text-white'} max-w-lg flex flex-col items-center justify-center p-[3rem] py-[5rem] rounded-2xl`}>
                                 <div className="p-2 mb-[2rem] bg-white rounded-[50%]">
-                                    {Lockicon == 'lock' ? <Lock size={50} className="text-orange-500 animate-bounce" /> : <LockOpen size={50} className="text-orange-500 animate-ping" /> }
+                                    {Lockicon == 'lock' ? <Lock size={50} className="text-orange-500 animate-bounce" /> : <LockOpen size={50} className="text-orange-500 animate-ping" />}
                                 </div>
                                 <h5 className="text-center text-2xl mb-5 text-white">This Event is Protected by a Password</h5>
                                 <h5 className="text-center text-sm text-white mb-5">Enter the shared Password for this event or contact the event organizers via {eventDetails.user.email}</h5>
@@ -433,80 +435,82 @@ function ViewEventComponent({ variation }) {
                         </div>
                     </>
                 ) :
-                    <>
-                        <div className="flex flex-col  items-center p-5">
-                            {eventDetails.event_image && <img src={eventDetails.event_image} className="w-full h-[30rem] object-cover lg:w-4/5 mb-5 rounded-[2rem]" alt="" />}
+                    <div className="px-3 md:px-0">
+                        <div className="flex flex-col  items-center p-1 md:p-5">
+                            {eventDetails.event_image && <img src={eventDetails.event_image} className="w-full h-[30rem] object-cover  max-w-7xl mb-5 rounded-xl" alt="" />}
                         </div>
-                        <div className="max-w-7xl mx-auto p-6">
-                            <Header theme={theme} eventDetails={eventDetails} ticketDetails={ticketDetails} id={1} />
-                            <HostInfo theme={theme} eventDetails={eventDetails} ticketDetails={ticketDetails} />
+                        <div className="
+                        ">
+                            <div className={` ${theme === 'dark' ? 'bg-[#000]' : 'bg-white'} mb-5 px-5  rounded-xl max-w-7xl mx-auto p-6 `} >
+                                <Header theme={theme} eventDetails={eventDetails} ticketDetails={ticketDetails} id={1} />
+                                <HostInfo theme={theme} eventDetails={eventDetails} ticketDetails={ticketDetails} />
 
-                            <div className="mb-5">
-                                <h2 className={`text-xl font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>About Event</h2>
-                                <p className={`${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
-                                    {eventDetails.event_description}
-                                </p>
-                            </div>
-                            <div className="mb-5">
-                                <h2 className={`text-l font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>Restrictions</h2>
-                                <p className={`${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>{eventDetails.additional_info && eventDetails.additional_info.restrictions}</p>
-                            </div>
-
-
-
-                            <div className="mb-8">
-                                {eventDetails.additional_info && eventDetails.additional_info.special_guests && eventDetails.additional_info.special_guests.trim() !== '' && (
-                                    <>
-                                        <h2 className={`text-l font-bold ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>
-                                            Special Guests
-                                        </h2>
-                                        <p className={`mt-2 ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
-                                            {eventDetails.additional_info.special_guests.split(',').map((guest, index) => (
-                                                <span key={index} className={`${theme === 'dark' ? 'bg-gray-100 text-gray-600' : 'bg-orange-500 text-white'}  px-4 py-2 mr-2 rounded-full text-sm`}>
-                                                    {guest.trim()}
-                                                    {index < eventDetails.additional_info.special_guests.split(',').length - 1 && ' '}
-                                                </span>
-                                            ))}
-                                        </p>
-                                    </>
-                                )}
-                            </div>
-
-
-
-
-                            {ticketDetails.days.map((day, index) => (
-                                <div key={index} className="mb-5 px-5 rounded-l border">
-
-                                    <div className={`flex items-center my-6 pb-2 `}>
-
-                                        <span className={`text-l mx-4 font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>Day {index + 1}</span>
-                                        <div className="flex-1 border-t border-gray-300"></div>
-                                    </div>
-
-                                    <DateTime theme={theme} eventDetails={eventDetails} ticketDetails={ticketDetails} index={index} day={day} />
-                                    <Location theme={theme} eventDetails={eventDetails} ticketDetails={ticketDetails} index={index} day={day} />
+                                <div className="mb-5">
+                                    <h2 className={`text-xl font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>About Event</h2>
+                                    <p className={`${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
+                                        {eventDetails.event_description}
+                                    </p>
                                 </div>
-                            ))
-                            }
-                            <div className="mb-8">
-                                <h2 className={`text-xl font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>Refund Policy</h2>
-                                <p className={`${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>No Refund</p>
-                            </div>
+                                <div className="mb-5">
+                                    <h2 className={`text-l font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>Restrictions</h2>
+                                    <p className={`${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>{eventDetails.additional_info && eventDetails.additional_info.restrictions}</p>
+                                </div>
+
+
+
+                                <div className="mb-8">
+                                    {eventDetails.additional_info && eventDetails.additional_info.special_guests && eventDetails.additional_info.special_guests.trim() !== '' && (
+                                        <>
+                                            <h2 className={`text-l font-bold ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>
+                                                Special Guests
+                                            </h2>
+                                            <p className={`mt-2 ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
+                                                {eventDetails.additional_info.special_guests.split(',').map((guest, index) => (
+                                                    <span key={index} className={`${theme === 'dark' ? 'bg-gray-100 text-gray-600' : 'bg-orange-500 text-white'}  px-4 py-2 mr-2 rounded-full text-sm`}>
+                                                        {guest.trim()}
+                                                        {index < eventDetails.additional_info.special_guests.split(',').length - 1 && ' '}
+                                                    </span>
+                                                ))}
+                                            </p>
+                                        </>
+                                    )}
+                                </div>
+
+
+
+
+                                {ticketDetails.days.map((day, index) => (
+                                    <div key={index} className={` ${theme === 'dark' ? 'bg-black' : 'bg-white'} mb-5 px-5  rounded-l border`}>
+
+                                        <div className={`flex items-center my-6 pb-2 `}>
+
+                                            <span className={`text-l mx-4 font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>Day {index + 1}</span>
+                                            <div className="flex-1 border-t border-gray-300"></div>
+                                        </div>
+
+                                        <DateTime theme={theme} eventDetails={eventDetails} ticketDetails={ticketDetails} index={index} day={day} />
+                                        <Location theme={theme} eventDetails={eventDetails} ticketDetails={ticketDetails} index={index} day={day} />
+                                    </div>
+                                ))
+                                }
+                                <div className="mb-8">
+                                    <h2 className={`text-xl font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>Refund Policy</h2>
+                                    <p className={`${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>No Refund</p>
+                                </div>
 
 
 
 
 
-                            <div className="mb-8">
-                                {eventDetails.additional_info && eventDetails.additional_info.authority_notification && (
-                                    <>
-                                        <h2 className={`text-l font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>Notified Authorities</h2>
-                                        <p className={`${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>{eventDetails.additional_info.authority_notification}</p></>
-                                )}
-                            </div>
+                                <div className="mb-8">
+                                    {eventDetails.additional_info && eventDetails.additional_info.authority_notification && (
+                                        <>
+                                            <h2 className={`text-l font-bold  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>Notified Authorities</h2>
+                                            <p className={`${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>{eventDetails.additional_info.authority_notification}</p></>
+                                    )}
+                                </div>
 
-                            {/* <div className={`flex flex-col items-center my-[3rem]`}>
+                                {/* <div className={`flex flex-col items-center my-[3rem]`}>
                         <div className="flex flex-col w-full px-2 my-5 items-start text-start">
                             <h2 className={`text-xl font-bold text-start  ${theme === 'dark' ? 'text-white' : 'text-[#040171]'}`}>More Events From this Organizer</h2>
 
@@ -574,11 +578,12 @@ function ViewEventComponent({ variation }) {
                             ))}
                         </div>
                     </div> */}
-                            <TicketModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} eventId={id} eventDetails={eventDetails}
-                                ticketDetails={ticketDetails} />
+                                <TicketModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} eventId={id} eventDetails={eventDetails}
+                                    ticketDetails={ticketDetails} />
 
+                            </div>
                         </div>
-                    </>
+                    </div>
             }
         </section>
     );
