ff55e949d9bc58d35920e0a1a503f84e2561f266
Kingsley Ihemelandu <kijuchihe@gmail.com>
2025-07-07 00:08:23 +0100

    Implement apis

--- src/shared/services/api/index.ts
+++ src/shared/services/api/index.ts
@@ -9,72 +9,71 @@ export * from "./authApi";
 export * from "./scanApi";
 export * from "./ticketsApi";
 export * from "./earningsApi";
+export * from "./organizerProfileApi";
 
 const newApi = axios.create({
-	baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
-	withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
+  withCredentials: true,
 });
 
 newApi.interceptors.request.use((request) => {
-	const token = Cookies.get("access_token");
-	if (token) {
-		request.headers.Authorization = `Bearer ${token}`;
-	}
-	return request;
+  const token = Cookies.get("access_token");
+  if (token) {
+    request.headers.Authorization = `Bearer ${token}`;
+  }
+  return request;
 });
 
 newApi.interceptors.response.use(
-	(response) => response,
-	async (error) => {
-		if (
-			error.response?.status === 401 &&
-			error.config &&
-			!error.config.__isRetry
-		) {
-			error.config._retryCount = error.config._retryCount || 0;
-			const maxRetries = 1; // Limit to 1 retry
+  (response) => response,
+  async (error) => {
+    if (
+      error.response?.status === 401 &&
+      error.config &&
+      !error.config.__isRetry
+    ) {
+      error.config._retryCount = error.config._retryCount || 0;
+      const maxRetries = 1; // Limit to 1 retry
 
-			if (error.config._retryCount >= maxRetries) {
-				// Max retries reached, redirect to login
-				localStorage.clear();
-				window.location.href = "/login";
-				return Promise.reject(error);
-			}
-			if (!error.config._isRetrying) {
-				error.config._isRetrying = true; // Mark as retrying
-				error.config._retryCount += 1;
-				try {
-					const accessToken = await authApi.refreshAccessToken();
-					error.config.headers[
-						"Authorization"
-					] = `Bearer ${accessToken}`;
-					return axios(error.config); // Retry original request
-				} catch (refreshError) {
-					// Logout or redirect to login
-					console.error("Token refresh failed, logging out");
-					Cookies.remove("access_token");
-					Cookies.remove("refresh_token");
-					window.location.href = "/login";
-					return Promise.reject(refreshError);
-				}
-			}
-		}
-		return Promise.reject(error);
-	}
+      if (error.config._retryCount >= maxRetries) {
+        // Max retries reached, redirect to login
+        localStorage.clear();
+        window.location.href = "/login";
+        return Promise.reject(error);
+      }
+      if (!error.config._isRetrying) {
+        error.config._isRetrying = true; // Mark as retrying
+        error.config._retryCount += 1;
+        try {
+          const accessToken = await authApi.refreshAccessToken();
+          error.config.headers["Authorization"] = `Bearer ${accessToken}`;
+          return axios(error.config); // Retry original request
+        } catch (refreshError) {
+          // Logout or redirect to login
+          console.error("Token refresh failed, logging out");
+          Cookies.remove("access_token");
+          Cookies.remove("refresh_token");
+          window.location.href = "/login";
+          return Promise.reject(refreshError);
+        }
+      }
+    }
+    return Promise.reject(error);
+  }
 );
 
 export const dashboardApi = {
-	getStats: async function (yearly: boolean = false) {
-		const res = await newApi.get(`/dashboard/stats?yearly=${yearly}`);
-		return res.data;
-	},
+  getStats: async function (yearly: boolean = false) {
+    const res = await newApi.get(`/dashboard/stats?yearly=${yearly}`);
+    return res.data;
+  },
 };
 
 export const categoriesApi = {
-	getCategories: async () => {
-		const res = await newApi.get("/categories");
-		return res.data;
-	},
+  getCategories: async () => {
+    const res = await newApi.get("/categories");
+    return res.data;
+  },
 };
 
 export default newApi;
--- src/shared/services/api/usersApi.ts
+++ src/shared/services/api/usersApi.ts
@@ -2,84 +2,86 @@ import newApi from ".";
 import { IUser } from "../../../types";
 
 export const usersApi = {
-	resolveAccount: async function (data: {
-		account_number: string;
-		bank_code: string;
-	}) {
-		const res = await newApi.post("/users/resolve-account", data);
-		return res.data;
-	},
-	async getMe() {
-		const res = await newApi.get("/users/me");
-		return res.data;
-	},
-	async updateUser(data: FormData) {
-		const res = await newApi.put("/users/me", data, {
-			headers: {
-				"Content-Type": "multipart/form-data",
-			},
-		});
-		return res.data;
-	},
-	async followUser(user_id: string) {
-		const res = await newApi.post("/users/follow", { user_id });
-		return res.data;
-	},
-	async unFollowUser(user_id: string) {
-		const res = await newApi.post("/users/unfollow", { user_id });
-		return res.data;
-	},
-	async getUserEventEarnings() {
-		const res = await newApi.get("/users/event-earnings");
-		return res.data;
-	},
-	async getUserFollowers() {
-		const res = await newApi.get("/users/followers");
-	},
-	async getUserFollowing() {
-		const res = await newApi.get("/users/following");
-	},
-	async getUserEarnings() {
-		const res = await newApi.get("/users/earnings");
-		return res.data;
-	},
-	async getUserAffiliateEarnings() {
-		const res = await newApi.get("/users/affiliate-earnings");
-		return res.data;
-	},
-	async getUserReferralEarnings() {
-		const res = await newApi.get("/users/referral-earnings");
-		return res.data;
-	},
-	async deleteUser({ reason }: { reason: string }) {
-		const res = await newApi.delete("/users/me", {
-			data: { reason },
-		});
-		return res.data;
-	},
-	updateAccountDetails: async function (data: {
-		account_number: string;
-		bank_code: string;
-		bank_name: string;
-		account_name: string;
-	}) {
-		const res = await newApi.put("/users/account-details", data);
-		return res.data;
-	},
-	requestWithdrawal: async function (data: { amount: number }) {
-		const res = await newApi.post("/users/withdraw", data);
-		return res.data;
-	},
-	getWithdrawals: async function () {
-		const res = await newApi.get("/users/withdrawals");
-		return res.data;
-	},
-	getEarnings: async function () {
-		const res = await newApi.get("/users/earnings");
-		return res.data;
-	},
-	async getTotalOrders() {
-		const res = await newApi.get("/users/total-orders");
-		return res.data;
-	},
+  resolveAccount: async function (data: {
+    account_number: string;
+    bank_code: string;
+  }) {
+    const res = await newApi.post("/users/resolve-account", data);
+    return res.data;
+  },
+  async getMe() {
+    const res = await newApi.get("/users/me");
+    return res.data;
+  },
+  async updateUser(data: FormData) {
+    const res = await newApi.put("/users/me", data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return res.data;
+  },
+  async followUser(user_id: string) {
+    const res = await newApi.post("/users/follow", { user_id });
+    return res.data;
+  },
+  async unFollowUser(user_id: string) {
+    const res = await newApi.post("/users/unfollow", { user_id });
+    return res.data;
+  },
+  async getUserEventEarnings() {
+    const res = await newApi.get("/users/event-earnings");
+    return res.data;
+  },
+  async getUserFollowers() {
+    const res = await newApi.get("/users/followers");
+    return res.data;
+  },
+  async getUserFollowing() {
+    const res = await newApi.get("/users/following");
+    return res.data;
+  },
+  async getUserEarnings() {
+    const res = await newApi.get("/users/earnings");
+    return res.data;
+  },
+  async getUserAffiliateEarnings() {
+    const res = await newApi.get("/users/affiliate-earnings");
+    return res.data;
+  },
+  async getUserReferralEarnings() {
+    const res = await newApi.get("/users/referral-earnings");
+    return res.data;
+  },
+  async deleteUser({ reason }: { reason: string }) {
+    const res = await newApi.delete("/users/me", {
+      data: { reason },
+    });
+    return res.data;
+  },
+  updateAccountDetails: async function (data: {
+    account_number: string;
+    bank_code: string;
+    bank_name: string;
+    account_name: string;
+  }) {
+    const res = await newApi.put("/users/account-details", data);
+    return res.data;
+  },
+  requestWithdrawal: async function (data: { amount: number }) {
+    const res = await newApi.post("/users/withdraw", data);
+    return res.data;
+  },
+  getWithdrawals: async function () {
+    const res = await newApi.get("/users/withdrawals");
+    return res.data;
+  },
+  getEarnings: async function () {
+    const res = await newApi.get("/users/earnings");
+    return res.data;
+  },
+  async getTotalOrders() {
+    const res = await newApi.get("/users/total-orders");
+    return res.data;
+  },
 };
